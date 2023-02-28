c40fc65e0702866368e32fd341b70df40d4e1a19
kaycmendes <kaycmendes@gmail.com>
2023-02-28 01:21:51 -0300

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
 // require('dotenv').config({ path: '../.env' })
-
+import Navbar from './Navbar.jsx';
 const bibleSearch = require('./api/index.js')
-
-
+import Cards from './Cards.jsx'
 
 const Home = () => {
-
   const [query, setQuery] = useState('');
-
   const [verse, setVerse] = useState('Ask, and it will be given to you; seek, and you will find; knock, and it will be opened to you');
   const [verseLocation, setVerseLocation] = useState('Matthew 7:7')
-
+  const [searchResults, setSearchResults] = useState([]);
+  const [isSearched, setIsSearched] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,42 +18,63 @@ const Home = () => {
     let verseData = data.verseData
     setVerse(verseData)
     setVerseLocation(locationData)
-    console.log("progress " + data)
+    setIsSearched(true);
+    setSearchResults([{ verse: verseData, verseLocation: locationData, query }, ...searchResults]);
+    setQuery('');
   };
-
-
 
   const handleInputChange = (e) => {
     const inputValue = e.target.value
     setQuery(inputValue)
   }
 
-
-
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen custom-bg">
-      <form onSubmit={handleSubmit} className="mb-4">
-        <input
-          type="text"
-          placeholder="Enter your question or subject"
-          value={query}
-          onChange={handleInputChange}
-          className="border p-2 rounded"
-        />
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded">Search</button>
-      </form>
-      <div className="relative rounded-lg overflow-hidden">
-        <div className="absolute inset-0 bg-gray-500 bg-blur"></div>
-        // inherit position inherit using tailwind
-        <div className="card p-8 z-10 relative text-center">
-          {(
-            <div className="card-content">
-              <p className="text-lg font-serif">{verse}</p>
-              <p className="text-xs text-black-500 relative z-11">{verseLocation}</p>
+    <div className="flex h-screen ">
+      <div className="w-4/5 custom-bg content-center text-center flex h-screen">
+        <div className='content-center m-auto '>
+          <Navbar />
+          <form onSubmit={handleSubmit} className="mb-4">
+            <h1 className="text-4xl font-bold text-center mb-8 text-slate-700">
+              What do the Scriptures say regarding:
+            </h1>
+            <input
+              type="text"
+              placeholder="Type here"
+              value={query}
+              onChange={handleInputChange}
+              className="border mb-5 p-3 rounded-l-xl custom-bg border-gray-800 search-bar text-slate-800"
+            />
+            <button type="submit" className="bg-slate-800 text-white p-3 rounded-r-xl">Search</button>
+          </form>
+          <div className="relative rounded-lg overflow-hidden">
+            <div className=" inset-0 bg-card bg-blur m-auto w-3/4 rounded-2xl">
+              <div className="card p-8 z-10 text-center">
+
+                {verse == '' ? (
+                  isSearched && (
+                    <div className="card-contentflex-col m-auto w-auto ">
+                    <p className="text-lg font-serif mb-3">Ask, and it will be given to you; seek, and you will find; knock, and it will be opened to you</p>
+                    <p className="flex-auto text-sm font-semibold text-black-500 relative z-11">Matthew 7:7</p>
+                  </div>
+                  )
+                ) : (
+                  <div className="card-contentflex-col m-auto w-auto ">
+                      <p className="text-lg font-serif mb-3">{verse}</p>
+                      <p className="flex-auto text-sm font-semibold text-black-500 relative z-11">{verseLocation}</p>
+                    </div>
+                )}
+
+
+
+              </div>
+
             </div>
-          )}
+
+          </div>
         </div>
+      </div>
+      <div className='w-1/5 custom-bg-left content-center text-center flex h-screen '>
+        {isSearched && <Cards results={searchResults} />}
       </div>
     </div>
   );
