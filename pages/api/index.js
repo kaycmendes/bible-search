e4882eaad1a17557dee8a5e0d369170ce45efff1
Kayc <38755587+kaycmendes@users.noreply.github.com>
2024-01-09 18:48:34 +0000

    open ai update

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,17 +1,14 @@
-const { Configuration, OpenAIApi } = require("openai");
+import OpenAI from 'openai';
 const { version } = require("react-dom");
 
 
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_KEY 
-});
-
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY // This is also the default, can be omitted
+  });
 
 async function bibleSearch(query) {
     const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo-1106",
+        model: "gpt-3.5-turbo",
         prompt:
             `Find a Bible verse "KJV only" that help find with the request:${query}. reply it as a JSON object with a 'verse' and 'location' variable, space between words, dont include the location inside the verse, IT HAS TO BE JSON"`,
         "max_tokens": 150
@@ -21,7 +18,7 @@ async function bibleSearch(query) {
     //old prompt Find a bible verse in the holy bible that talks about or mention this subject, ${query}." 
       //output ONLY as JSON object 2 var, verse named "verse" and verse location named "location", extrmely important: be short and no whitespace
 
-    let data = completion.data.choices[0].text
+    let data = completion.choices[0].text
     let verseData = await JSON.parse(data)
     let locationData = await JSON.parse(data)
     locationData = await verseData.location
